dc033f0ff50865cb5d56174f77dd8001d5f67407
Greg DeVore <greg.h.devore@gmail.com>
2020-01-01 11:31:28 -0800

    Completed github user activity

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ import requests
 # (or another user name)
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: main.py username')
+        sys.exit()
+
     username = sys.argv[1]
-
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
-
-
+    response = requests.get('https://api.github.com/users/{}/events'.format(username))
+    if response.status_code == 200: # Make sure OK response received
+        latest_event = response.json()[0]
+        event_id = latest_event['id']
+        event_type = latest_event['type']
+        event_timestamp = latest_event['created_at']
+        print('User {} created event {} of type \'{}\' at {}'.format(username,
+                                                                     event_id,
+                                                                     event_type,
+                                                                     event_timestamp))
+    else:
+        print('Error, response code of {:d} received. Unable to proceed'.format(response.status_code))
